3e0523034738e489482eb957ee51aaa8dc1e3b5a
Tom Lepard <t@Toms-MBP.broadband>
2020-05-25 16:42:15 +0100

    gets line, checks line, index to [row,col] n back

--- connectfour.js
+++ connectfour.js
@@ -1,6 +1,11 @@
-function calculateWinner(squares, lastClicked) {
-  const startOfRow = Math.floor(lastClicked / 7) * 7;
-  const endofRow = startOfRow + 6;
+const colCount = 7;
+const rowCount = 6;
+const winningLineLength = 4;
+function calculateWinner(squares, lastClicked) {}
+
+function checkRow(squares, lastClicked) {
+  const startOfRow = Math.floor(lastClicked / colCount) * colCount;
+  const endofRow = startOfRow + (colCount - 1);
   for (let i = startOfRow; i < endofRow; ++i) {
     if (
       squares[i] &&
@@ -8,17 +13,86 @@ function calculateWinner(squares, lastClicked) {
       squares[i] == squares[i + 2] &&
       squares[i] == squares[i + 3]
     ) {
-    
-      //console.log(squares[i].toString());
-    return [
-        squares[i],
-        [i, i + 1, i + 2, i + 3],
-      ];
-      //return(["playerx",[0,1,2,3]]);
-    } else {
-      return [null, [null]];
+      return [squares[i], [i, i + 1, i + 2, i + 3]];
     }
-  }
+  } //end of for loop
+  return [null, [null]];
 }
 
-module.exports = calculateWinner;
+function checkCol(squares, lastClicked) {
+  const startOfCol = lastClicked % colCount;
+  const endOfCol = startOfCol + (rowCount - 1);
+  for (let i = startOfCol; i < endOfCol; i += colCount) {
+    if (
+      squares[i] &&
+      squares[i] == squares[i + colCount] &&
+      squares[i] == squares[i + 2 * colCount] &&
+      squares[i] == squares[i + 3 * colCount]
+    )
+      return [
+        squares[i],
+        [i, i + colCount, i + 2 * colCount, i + 3 * colCount],
+      ];
+  }
+}
+function indexToCoords(index) {
+  let col = index % colCount;
+  let row = Math.floor(index / colCount);
+  return [row, col];
+}
+
+function coordsToIndex([row, col]) {
+  index = row * colCount + col;
+  return index;
+}
+
+function findStart(index, [rowStep, colStep]) {
+  var potentialWinner = [];
+  [currentRow, currentCol] = indexToCoords(index);
+  let rowPos = currentRow,
+    colPos = currentCol;
+  for (
+    rowPos, colPos;
+    rowPos >= 0 &&
+    rowPos > currentRow - winningLineLength &&
+    colPos >= 0 &&
+    colPos > currentCol - winningLineLength;
+    rowPos -= rowStep, colPos -= colStep
+  ) {}
+  return [rowPos, colPos];
+}
+function getLine([rowPos, colPos], [rowStep, colStep]) {
+  var potentialWinner = [];
+  for (
+    rowPos, colPos;
+    rowPos < rowCount && colPos < colCount && colPos >= 0;
+    rowPos += rowStep, colPos += colStep
+  ) {
+    potentialWinner.push(coordsToIndex([rowPos, colPos])); //array of line to be checked
+  }
+  return potentialWinner;
+}
+
+function checkLine(potentialWinner, squares) {
+  var winningLine = [];
+
+  for (let i = 0; i < potentialWinner.length; ++i) {
+    if (winningLine.length == 0 && squares[potentialWinner[i]]) {
+      winningLine.push(potentialWinner[i]);
+    } else if (squares[potentialWinner[i]] == squares[potentialWinner[i - 1]]) {
+      winningLine.push(potentialWinner[i]);
+    } else if (winningLine.length < winningLineLength) {
+      winningLine = [];
+      winningLine.push(potentialWinner[i]);
+    }
+  }
+  return winningLine;
+}
+
+module.exports = {
+  coordsToIndex,
+  indexToCoords,
+  findStart,
+  getLine,
+  checkLine,
+};
--- test/connectfour.spec.js
+++ test/connectfour.spec.js
@@ -1,41 +1,106 @@
-let calculateWinner = require("../connectfour");
-let rows = 6;
-let cols = 7;
-/*test("if win, calculateWinner(squares, 0) should return [playerx, winningLine]", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[1] = squares[2] = squares[3] = squares[0] = "playerx"; //setting up winning line, squares[0] to squares[3].
-  expect(calculateWinner(squares, 0)).toStrictEqual(["playerx", [0, 1, 2, 3]]);
-});
-test("correct row taken", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[1] = squares[2] = squares[3] = squares[0] = "playerx"; //setting up winning line, squares[0] to squares[3].
-  expect(calculateWinner(squares, 0)).toStrictEqual(["playerx", [0, 1, 2, 3]]);
+const calculateWinner = require("../connectfour");
+const checkCol = require("../connectfour");
+const {
+  coordsToIndex,
+  indexToCoords,
+  findStart,
+  getLine,
+  checkLine,
+} = require("../connectfour");
+
+const rows = 6;
+const cols = 7;
+const squares = new Array(rows * cols).fill(null);
+
+/*describe("can check any row with last clicked in any position", () => {
+  test("if win, calculateWinner(squares, 0) should return ['playerx', [0,1,2,3]]", () => {
+    squares[1] = squares[2] = squares[3] = squares[0] = "playerx"; //setting up winning line
+    const [player, cells] = calculateWinner(squares, 0);
+    expect(player).toMatch("playerx");
+    expect(cells).toStrictEqual([0, 1, 2, 3]);
+  });
+  test("if win, calculateWinner(squares, 10) should return ['playerx', [9,10,11,12]]", () => {
+    squares[9] = squares[10] = squares[11] = squares[12] = "playerx"; //setting up winning line
+    const [player, cells] = calculateWinner(squares, 10);
+    expect(player).toMatch("playerx");
+    expect(cells).toStrictEqual([9, 10, 11, 12]);
+  });
+  test("if win, calculateWinner(squares, 41) should return ['playerx', [38,39,40,41]]", () => {
+    squares[38] = squares[39] = squares[40] = squares[41] = "playerx"; //setting up winning line
+    const [player, cells] = calculateWinner(squares, 41);
+    expect(player).toMatch("playerx");
+    expect(cells).toStrictEqual([38, 39, 40, 41]);
+  });
+  test("if no win, calculateWinner(squares, 0) should return [null, [null]]", () => {
+    squares[0] = "playery";
+    expect(calculateWinner(squares, 0)).toBeNull;
+  });
 });
 
-test("if win, calculateWinner(squares, 0) should return [playerx, winningLine]", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[1] = squares[2] = squares[3] = squares[0] = "playerx"; //setting up winning line, squares[0] to squares[3].
-  expect(calculateWinner(squares, 0)).toStrictEqual(["playerx", [0, 1, 2, 3]]);
+
+describe("can check any column with last clicked in any position", () => {
+  test("if win, col 1 & lastClicked squares[8], returns ['playerx',[1,8,15,22]", () => {
+    squares[1] = squares[8] = squares[15] = squares[22] = "playerx"; //setting up winning line
+    const [player, cells] = checkCol(squares, 8);
+    expect(player).toMatch("playerx");
+    expect(cells).toStrictEqual([1,8,15,22]);
+  });
+});
+*/
+
+test.todo("checkWinner");
+
+describe("indexToCoords(index) returns [rowPosition,colPosition]", () => {
+  test("indexToCoords(0) returns [0,0]", () => {
+    expect(indexToCoords(0)).toStrictEqual([0, 0]);
+  });
+  test("indexToCoords(41) returns [5,6]", () => {
+    expect(indexToCoords(41)).toStrictEqual([5, 6]);
+  });
 });
 
-test("if no win, calculateWinner(squares, 0) should return [null, [null]]", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[0] = "playery";
-  expect(calculateWinner(squares, 0)).toBeNull
+describe("coordsToIndex([rowPosition,colPosition]) returns index", () => {
+  test("coordsToIndex([0,0]) returns 0", () => {
+    expect(coordsToIndex([0, 0])).toStrictEqual(0);
+  });
+  test("coordsToIndex([5,6]) returns 41", () => {
+    expect(coordsToIndex([5, 6])).toStrictEqual(41);
+  });
+  test("coordsToIndex([1,0]) returns 7", () => {
+    expect(coordsToIndex([1, 0])).toStrictEqual(7);
+  });
 });
 
-test("win any row w/ any position of last clicked e.g. 1", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[1] = squares[2] = squares[3] = squares[0] = "playerx"; //setting up winning line, squares[0] to squares[3].
-  const [player, cells] = calculateWinner(squares, 1);
-  expect(player).toMatch('playerx');
-  expect(cells).toStrictEqual([0, 1, 2, 3]);
-});*/
-test("win any row w/ any position of last clicked(10)", () => {
-  let squares = new Array(rows * cols).fill(null);
-  squares[11] = squares[8] = squares[9] = squares[10] = "playerx"; //setting up winning line, squares[0] to squares[3].
- const [player, cells] = calculateWinner(squares, 10);
- expect(player).toMatch('playerx');
- expect(cells).toStrictEqual([8,9,10,11]);
+describe("findStart(index, [rowStep,colStep]) returns [rowPos,colPos]", () => {
+  test("findStart(6, [0, 1]) returns [0, 2]", () => {
+    expect(findStart(6, [0, 1])).toStrictEqual([0, 2]);
+  });
 });
 
+describe("getLine([rowPos, colPos], [rowStep,colStep]) returns [potentialWinningLine]", () => {
+  test("HORIZONTAL: getLine([0,3], [0, 1]) returns [3, 4, 5, 6]", () => {
+    expect(getLine([0, 3], [0, 1])).toStrictEqual([3, 4, 5, 6]);
+  });
+  test("VERTICAL: getLine([0,3], [1, 0]) returns [3, 10, 17, 24, 31, 38]", () => {
+    expect(getLine([0, 3], [1, 0])).toStrictEqual([3, 10, 17, 24, 31, 38]);
+  });
+  test("DIAGNAL SE: getLine([0,3], [1, 1]) returns [3, 11, 19, 27]", () => {
+    expect(getLine([0, 3], [1, 1])).toStrictEqual([3, 11, 19, 27]);
+  });
+  test("DIAGNAL SW: getLine([0,3], [1, -1]) returns [3, 9, 15, 21]", () => {
+    expect(getLine([0, 3], [1, -1])).toStrictEqual([3, 9, 15, 21]);
+  });
+});
+
+describe("checkLine([potentialWinningLine]) returns [winningLine] or [null]", () => {
+  test("checkLine([0, 1, 2, 3, 4, 5, 6]) returns [2, 3, 4, 5]", () => {
+    squares[0] = squares[1] = squares[6] = "playerx";
+    squares[3] = squares[2] = squares[4] = squares[5] = "playery"; //setting up winning line
+    expect(checkLine([0, 1, 2, 3, 4, 5, 6], squares)).toStrictEqual([
+      2,
+      3,
+      4,
+      5,
+    ]);
+  });
+});
